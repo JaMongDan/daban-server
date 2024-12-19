6ac7fec5d54594aab9dda8f277e9ff20f4e25bec
tddudwns1 <tjddudwns1@naver.com>
2024-12-20 06:15:05 +0900

    🎉 Tada: daban backend project 생성

---
+++ src/main/java/com/jamongdan/daban/DabanApplication.java
@@ -0,0 +1,13 @@
+package com.jamongdan.daban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DabanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamongdan/daban/DabanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamongdan.daban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DabanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
